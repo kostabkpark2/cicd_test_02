9398b2fb47b04b07be578a46ebd5b3283b5b4c5f
kostabkpark2 <kostabkpark2@gmail.com>
2025-12-01 11:54:31 +0900

    cicd test 02

---
+++ src/main/java/org/example/cicd_test_02/CicdTest02Application.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTest02Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdTest02Application.class, args);
+  }
+
+}
